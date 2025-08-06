041e0b365533d9037eed59acf2fc2f077a31e14e
22053118 <22053118@kiit.ac.in>
2025-08-07 01:08:04 +0530

    recommit

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -38,8 +38,9 @@ mongoose
   })
   .then(() => {
     console.log('✅ MongoDB connected');
-    app.listen(5000, () => {
-      console.log('🚀 Server running on http://localhost:5000');
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
