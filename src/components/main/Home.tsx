6c406545fe9bb4ae5325a1ee175fe31d6213dc0c
22053118 <22053118@kiit.ac.in>
2026-02-16 20:53:45 +0530

    made changes for response

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -56,7 +56,7 @@ const Home = () => {
     };
 
     try {
-      const res = await fetch('${process.env.REACT_APP_API_URL}/api/estimate', {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/estimate`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
